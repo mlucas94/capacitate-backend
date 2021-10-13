e45749db0fbe9fd9eaf834e8b5f268ca363f1274
Lucas Morón <lucasmoron0512@gmail.com>
2021-10-13 00:33:55 -0300

    Feat: Authentication agregada con user hardcodeado

---
+++ src/main/java/ar/edu/unq/ttip/grupo9s22021/backend/capacitatebackend/service/auth/JwtUserDetailsService.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.ttip.grupo9s22021.backend.capacitatebackend.service.auth;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("capacitate".equals(username)) {
+            return new User("capacitate", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
