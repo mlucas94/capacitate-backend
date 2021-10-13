43e4d35d09ccb987ad5c89ce95299b345e81178c
Lucas Morón <lucasmoron0512@gmail.com>
2021-10-13 20:47:13 -0300

    Refact: endpoint de todos los cursos se puede acceder sin autenticar.

--- src/main/java/ar/edu/unq/ttip/grupo9s22021/backend/capacitatebackend/config/SecurityConfig.java
+++ src/main/java/ar/edu/unq/ttip/grupo9s22021/backend/capacitatebackend/config/SecurityConfig.java
@@ -50,10 +50,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                .authorizeRequests().antMatchers("/authenticate", "/api/curso").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
